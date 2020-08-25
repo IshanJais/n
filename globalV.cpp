83dbb3805d0c15bdc4c18b4ecf3254ff1d01be41
nainachavhan <70107086+nainachavhan@users.noreply.github.com>
2020-08-26 00:11:34 +0530

    Add files via upload

---
+++ globalV.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+ int value { 5 };
+
+int main()
+
+{
+	
+	int value { 7 };
+    value;
+	
+	::value;
+	std::cout<<"local variable"<<value<<'\n'; 
+	std::cout << "global  variable" << ::value << '\n';
+	return 0;
+ }
