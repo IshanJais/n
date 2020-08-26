2678211e55ba1b7f9b0f01def57414447775d6c8
nainachavhan <70107086+nainachavhan@users.noreply.github.com>
2020-08-26 11:26:00 +0530

    Add files via upload

---
+++ narrow.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+void printvalue(int x)
+{
+	std::cout << "the x=" << x << '\n';
+
+}
+
+int main()
+{
+	int  y = 36.8;
+	std::cout << "the y =" << y << '\n';
+	printvalue  (32.4 );
+	return 0;
+}
